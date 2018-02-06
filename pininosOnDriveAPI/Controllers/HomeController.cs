5496475706211bb9de4c759efb63b099d8b450d0
Fernando Canul Centeno <fernando.canul@nearshoretechnology.com>
2018-02-06 16:28:42 -0600

    add CREATE and READ function from DriveAPI with owner google account

--- pininosOnDriveAPI/Controllers/HomeController.cs
+++ pininosOnDriveAPI/Controllers/HomeController.cs
@@ -5,6 +5,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using pininosOnDriveAPI.Models;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Newtonsoft.Json;
+using Data = Google.Apis.Sheets.v4.Data;
+using System.IO;
+using Google.Apis.Sheets.v4.Data;
+using System.Threading;
+using Google.Apis.Util.Store;
 
 namespace pininosOnDriveAPI.Controllers
 {
@@ -18,6 +27,98 @@ namespace pininosOnDriveAPI.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+
+            // If modifying these scopes, delete your previously saved credentials
+            // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
+            string[] Scopes = { SheetsService.Scope.Spreadsheets };
+            string ApplicationName = "Google Sheets API .NET Quickstart";
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/sheets.googleapis.com-dotnet-quickstart.json");
+                //string credPath = Directory.GetCurrentDirectory() + "\\client_secret.json";
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define request parameters.
+            String spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
+            String range = "Class Data!A2:E";
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                    service.Spreadsheets.Values.Get(spreadsheetId, range);
+
+            // Prints the names and majors of students in a sample spreadsheet:
+            // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+            ValueRange response = request.Execute();
+            IList<IList<Object>> values = response.Values;
+            if (values != null && values.Count > 0)
+            {
+                Console.WriteLine("Name, Major");
+                foreach (var row in values)
+                {
+                    // Print columns A and E, which correspond to indices 0 and 4.
+                    Console.WriteLine("{0}, {1}", row[0], row[4]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data found.");
+            }
+            //Another service, this is to CREATE
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/sheets.googleapis.com-dotnet-quickstart.json");
+                //string credPath = Directory.GetCurrentDirectory() + "\\client_secret.json";
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets API service.
+            service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });            
+
+            // TODO: Assign values to desired properties of `requestBody`:
+            Data.Spreadsheet requestBody = new Data.Spreadsheet();
+            requestBody.Properties.Title = "PininosOnSpreadSheetWithDriveAPI";
+            requestBody.Properties.ETag = "FerchoLio";            
+
+            SpreadsheetsResource.CreateRequest requestTwo = service.Spreadsheets.Create(requestBody);
+
+            // To execute asynchronously in an async method, replace `request.Execute()` as shown:
+            Data.Spreadsheet responseTwo = requestTwo.Execute();
+            // Data.Spreadsheet response = await request.ExecuteAsync();
+
+            // TODO: Change code below to process the `response` object:
+            //Console.WriteLine(JsonConvert.SerializeObject(response));            
 
             return View();
         }
@@ -33,5 +134,17 @@ namespace pininosOnDriveAPI.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-    }
+
+        public static UserCredential GetCredential()
+        {
+            // TODO: Change placeholder below to generate authentication credentials. See:
+            // https://developers.google.com/sheets/quickstart/dotnet#step_3_set_up_the_sample
+            //
+            // Authorize using one of the following scopes:
+            //     "https://www.googleapis.com/auth/drive"
+            //     "https://www.googleapis.com/auth/drive.file"
+            //     "https://www.googleapis.com/auth/spreadsheets"
+            return null;
+        }
+    }   
 }
